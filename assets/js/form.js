69e75a4aeba752e4c7818632aef80ddede4e0bd1
Tess McGovern <ren.mcgovern@gmail.com>
2024-06-11 22:12:29 -0400

    successfull save to local storage

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,17 +1,51 @@
 const submitBtn = document.getElementById('submitEL');
 const blogForm = document.getElementById('blogForm');
+const usernameInput = document.getElementById('username');
+const titleInput = document.getElementById('title');
+const blogContentInput = document.getElementById('blogContent');
+const displayPost = document.getElementById('displayPost');
 
-let post = localStorage.getItem('post');
+// function renderLastBlogPost() {
 
-blogForm.textContent = post;
+    // const post = localStorage.getItem('post');
+//     if (post) {
+//         displayPost.innerHTML = post;
+//     }
+// }
 
 
-
-submitBtn.addEventListener('click', function(event) {
+submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
-    console.log('click');
 
-    
-})
+    const username = usernameInput.value;
+    const title = titleInput.value;
+    const blogContent = blogContentInput.value;
+
+    if (!username || !title || !blogContent) {
+        alert('Please fill in all fields before submitting!');
+        return;
+    }
+
+    const postBlog = {
+        username: username,
+        title: title,
+        blogContent: blogContent,
+    };
+
+    let posts = JSON.parse(localStorage.getItem('posts')) || [];
+    posts.push(postBlog);
+
+    localStorage.setItem('posts', JSON.stringify(posts));
+
+
+    usernameInput.value = '';
+    titleInput.value = '';
+    blogContentInput.value = '';
+
+    alert('Post successfully saved');
+});
+
+// renderLastBlogPost();
+
 
 
