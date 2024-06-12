b6ba0a9e79e596bbe1ed96b2a8073c58a3eae7da
Tess McGovern <ren.mcgovern@gmail.com>
2024-06-12 17:24:59 -0400

    added commentary to index.js and added window location change

---
+++ assets/js/blog.js
@@ -0,0 +1,3 @@
+const body = document.body;
+const title = document.createElement('h1');
+const username = document.createElement('h2');
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,51 +1,59 @@
+// retrieve DOM elements by their ID (referencing indexhtml)
 const submitBtn = document.getElementById('submitEL');
-const blogForm = document.getElementById('blogForm');
 const usernameInput = document.getElementById('username');
 const titleInput = document.getElementById('title');
 const blogContentInput = document.getElementById('blogContent');
-const displayPost = document.getElementById('displayPost');
-
-// function renderLastBlogPost() {
-
-    // const post = localStorage.getItem('post');
-//     if (post) {
-//         displayPost.innerHTML = post;
-//     }
-// }
 
 
 submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
 
+    // retrieving input values from the blog form
     const username = usernameInput.value;
     const title = titleInput.value;
     const blogContent = blogContentInput.value;
 
+    //if any of these are left empty/equal nothing then this alert will show
     if (!username || !title || !blogContent) {
         alert('Please fill in all fields before submitting!');
         return;
     }
 
+    // this creates an object for our blog post consisting of 
+    // the username, title, and content
     const postBlog = {
         username: username,
         title: title,
         blogContent: blogContent,
     };
 
+    // here we grab the previous posts/objects from the local storage,
+    // or we create and empty array to add to if there is none currently existing
+    // the .push adds our newest object to this array
     let posts = JSON.parse(localStorage.getItem('posts')) || [];
     posts.push(postBlog);
 
+    // then we save the new array with the added post to local storage
     localStorage.setItem('posts', JSON.stringify(posts));
 
-
+    // clear input values bc we did an event prevent default before to stop
+    // the page from auto reloading/resetting on submit
     usernameInput.value = '';
     titleInput.value = '';
     blogContentInput.value = '';
 
     alert('Post successfully saved');
+
+    // change window location upon submit
+    window.location.href = 'blog.html';
 });
 
-// renderLastBlogPost();
+// we use .parse and .stringify when retrieving and adding data 
+// bc local storage can only deal with strings
+// but arrays and objects are more complex
+
+
+
 
 
 
