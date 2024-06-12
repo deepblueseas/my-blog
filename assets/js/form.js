f247bd7be8d7d4a4b11de2627ae17b2295516027
Tess McGovern <ren.mcgovern@gmail.com>
2024-06-11 21:40:20 -0400

    submit button registers a click

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,54 +1,17 @@
 const submitBtn = document.getElementById('submitEL');
-const usernameInput = document.getElementById('username');
-const titleInput = document.getElementById('title');
-const blogContentInput = document.getElementById('blogContent');
-const msgDiv = document.createElement('div');
+const blogForm = document.getElementById('blogForm');
+
+let post = localStorage.getItem('post');
+
+blogForm.textContent = post;
 
 
-renderLastBlogPost();
-
-function displayMessage(type, message){
-    msgDiv.textContent = message;
-    msgDiv.setAttribute('class', type);
-};
-
-    function renderLastBlogPost(){
-        const username = localStorage.getItem('username');
-        const title = localStorage.getItem('title');
-        const blogContent = localStorage.getItem('blogContent');
-
-    if (!username || !title || !blogContent) {
-        return;
-    }
-
-    usernameInput.value = username;
-    titleInput.value = title;
-    blogContentInput.value = blogContent;
-
-    displayMessage('success', 'Post uploaded');
-}
 
 submitBtn.addEventListener('click', function(event) {
     event.preventDefault();
+    console.log('click');
 
-    const username = usernameInput.value;
-    const title = titleInput.value;
-    const blogContent = blogContentInput.value;
-
-    if (!username || !title || !blogContent) {
-        displayMessage('error', 'Please fill in all fields before submitting');
-        return;
-    }
-
-    localStorage.setItem('username', username);
-    localStorage.setItem('title', title);
-    localStorage.setItem('blogContent', blogContent);
-
-    renderLastBlogPost();
-
-    displayMessage('success', 'Post saved');
-});
-
-
+    
+})
 
 
